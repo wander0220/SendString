23b4ac06321c3284b647c7e52ee766e6eb6cd03d
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-09-29 10:12:12 +0900

    delegate 연결, 연결 해제

--- SendString/Program.cs
+++ SendString/Program.cs
@@ -8,9 +8,28 @@ namespace SendString
 {
     class Program
     {
-        public delegate void SendString(string messenge)
+        public delegate void SendString(string messenge);
         static void Main(string[] args)
         {
+            SendString sayHello, sayGoodbye, multiDelegate;
+            sayHello = Hello;
+            sayGoodbye = Goodbye;
+
+            multiDelegate = sayHello + sayGoodbye;
+            multiDelegate("윤인성");
+
+            Console.WriteLine();multiDelegate -= sayGoodbye; //sayGoodbye 연결 해제
+            multiDelegate("윤인성");
+        }
+
+        private static void Goodbye(string messenge)
+        {
+            Console.WriteLine("안녕하세요~ " + messenge + "씨...!");
+        }
+
+        private static void Hello(string messenge)
+        {
+            Console.WriteLine("안녕히가세요~ " + messenge + "씨...!");
         }
     }
 }
