ba67adacfdfff5d30f87da5b2362400496e487d0
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-09-29 10:04:54 +0900

    초기 커밋.

---
+++ SendString/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendString
+{
+    class Program
+    {
+        public delegate void SendString(string messenge)
+        static void Main(string[] args)
+        {
+        }
+    }
+}
